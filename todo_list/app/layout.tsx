6006e1541fa98a9d984e0e933f78a24d8a3b78d7
BakhtawarAbbasi <bakhtawarabbasi009@gmail.com>
2024-11-12 21:17:39 +0500

    todo list

--- todo_list/app/layout.tsx
+++ todo_list/app/layout.tsx
@@ -1,9 +1,19 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+
 import { ThemeProvider } from "./context/ThemeContext";
 
-
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff", // Updated path if needed
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff", // Updated path if needed
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +23,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
